433a131285e73872a1841b546ca66f3e25f30360
arthur999999 <arthurhono.contato@gmail.com>
2022-09-19 17:04:28 -0300

    like contagem

--- src/post.js
+++ src/post.js
@@ -4,16 +4,20 @@ export default function Post () {
     const [likeStatus2, setLikeStatus2] = React.useState('heart-outline')
     const [saveStatus1, setSaveStatus1] = React.useState('bookmark-outline')
     const [saveStatus2, setSaveStatus2] = React.useState('bookmark-outline')
+    const [likeQ1, setLikeQ1] = React.useState(101523)
+    const [likeQ2, setLikeQ2] = React.useState(99159)
     const postData = [{save: saveStatus1, saveSet: setSaveStatus1,  like: likeStatus1, likeset: setLikeStatus1, userName: 'meowed', userImg: './assets/img/meowed.svg', postImg: './assets/img/gato-telefone.svg', 
-        postLike: '101.523', imgLike: './assets/img/respondeai.svg', nameLike: 'respondeai'}, {save: saveStatus2, saveSet: setSaveStatus2, like: likeStatus2, likeset: setLikeStatus2, userName: 'barked', userImg: './assets/img/barked.svg', postImg: './assets/img/dog.svg', 
-        postLike: '99.159', imgLike: './assets/img/adorable_animals.svg', nameLike: 'adorable_animals'}]
+        postLike: likeQ1, postLikeset: setLikeQ1, imgLike: './assets/img/respondeai.svg', nameLike: 'respondeai'}, {save: saveStatus2, saveSet: setSaveStatus2, like: likeStatus2, likeset: setLikeStatus2, userName: 'barked', userImg: './assets/img/barked.svg', postImg: './assets/img/dog.svg', 
+        postLike: likeQ2, postLikeset: setLikeQ2, imgLike: './assets/img/adorable_animals.svg', nameLike: 'adorable_animals'}]
     
-    function likeOptions (a, b) {
+    function likeOptions (a, b, c, d) {
       if(a == 'heart-outline'){
         b('heart-sharp')
+        c(d + 1)
       }
       if(a == 'heart-sharp'){
         b('heart-outline')
+        c(d - 1)
       }
     }
     return (
@@ -35,7 +39,7 @@ export default function Post () {
         <div class="fundo">
           <div class="acoes">
             <div>
-              <ion-icon onClick={()=> likeOptions(pos.like, pos.likeset)} name={pos.like}></ion-icon>
+              <ion-icon onClick={()=> likeOptions(pos.like, pos.likeset, pos.postLikeset, pos.postLike)} name={pos.like}></ion-icon>
               <ion-icon name="chatbubble-outline"></ion-icon>
               <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
